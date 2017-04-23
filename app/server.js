2ab1438493a6443c0b4d3db7844f90ed72f8369c
Joshua Hackett <hello@joshuahackett.com>
2017-04-23 01:35:47 +0100

    Core server side code finished

--- app/server.js
+++ app/server.js
@@ -1,32 +1,135 @@
 //Get the necessary modules
 var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var express = require('express');
 // Get the right express method
 var app = express();
 // Get the settings data file
 var settings = require('./settings.json');
+// Get the recordings metadata file
+var metadata = require('./metadata.json');
+
 // Load in vars from the settings file
 var url = settings.streamUrl;
-var duration = settings.logDuration;
-var offset = settings.logOffset;
+var targetDuration = settings.logDuration*60; //In milliseconds
+var archiveLength = settings.archiveLength;
+var stationName = settings.stationName.toLowerCase();
 
-// Create the writable stream for the output
-var wstream = fs.createWriteStream('app/public/recordings/output.mp3');
-
-// Check if the recordings directory exists, otherwise create it
-if (!fs.existsSync('./recordings')) {
-  fs.mkdirSync('./recordings');
+// Function to clear out old files if the number of recordings is greater than the number specified in the settings
+function deleteOldFiles(){
+  // If the number of elements in the metadata array is more than 750, delete the oldest/first file
+  if (metadata.recordings.length > archiveLength) {
+    // Get the filename of the first/oldest file from the object
+    var fileName = metadata.recordings[0].fileName;
+    var deletePath = './app/public/recordings/'+fileName;
+    // Perform the deletion
+    fs.unlink(deletePath, function(){
+      console.log("Deleting oldest recording: "+ fileName);
+    })
+    // Pop the first object off the array for good measure
+    metadata.recordings.shift();
+    var newData = JSON.stringify(metadata);
+    // // And write the new data to the file
+    fs.writeFile('./app/metadata.json',newData, function(err){
+        if (err) throw err;
+    });
+  }
 }
 
-//Get the data, save it to file and log it to the console
-http.get(url, function(response) {
-  // Save the data to a stream
-  response.pipe(wstream);
-  // WHen you finish, say so
-  response.on('end', function() {
-    console.log('FINISHED STREAMING');
+// Helper function to format the date
+function pad(n){return n<10 ? '0'+n : n}
+
+// Record and store a single log hour
+function record(duration){
+  // A blank default object to store the metadata
+  var meta = {
+    "id":0,
+    "txTime":"",
+    "txDate":"",
+    "showName":"",
+    "fileName":""
+  };
+  // Options for the below request
+  var options = {
+    host: 'faraday.smokeradio.co.uk',
+    port: 443,
+    path: '/api/live-info',
+    method: 'GET',
+    "rejectUnauthorized": false
+  };
+  // Make the request and update the var
+  var req = https.request(options, function(res) {
+    // Blank var to store response body
+    var body = '';
+    // Fill up the body var with the response chunk by chunk
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+    // At the end of the asynchronous request, execute this callback
+    res.on('end', function(){
+      // Calculate ID by iterating ID of previous file
+      if (metadata.recordings[0]) {
+        var id = metadata.recordings[metadata.recordings.length-1].id+1;
+      } else {
+        var id = 1;
+      }
+      //
+      body = JSON.parse(body).currentShow;
+      if (body.currentShow == undefined) {
+        var showName = "Jukebox";
+      } else {
+        var showName = body.currentShow[0].name;
+      }
+      // Get the current server date/time
+      var now = new Date();
+      // Populate the meta object
+      meta.id = id;
+      meta.txTime = String(pad(now.getHours())) + String(pad(now.getMinutes())) + String(pad(now.getSeconds()));
+      meta.txDate = String(pad(now.getDate())) + String(pad(now.getMonth()+1)) + String(now.getFullYear());
+      meta.showName = showName;
+      meta.fileName = stationName + "_" + meta.txTime + "_" + meta.txDate + "_" + meta.showName + ".mp3";
+      // Append this object to the existing metadata
+      metadata.recordings.push(meta);
+      var newData = JSON.stringify(metadata);
+      // And overwrite the existing metadata file with the new data
+      fs.writeFile('./app/metadata.json',newData, function(err){
+          if (err) throw err;
+      });
+      // Create the writable stream for the output, based on the filename calculated above
+      var wstream = fs.createWriteStream('app/public/recordings/' + meta.fileName);
+
+      //Get the data, save it to file and log it to the console
+      var record = http.get(url, function(response) {
+        // Save the data to a stream
+        response.pipe(wstream);
+        // Make sure the archive rolls over by deleting the oldest files
+        deleteOldFiles();
+        // WHen you finish, say so
+        response.on('end', function() {
+          console.log('Recording paused');
+        });
+      });
+
+      // After a specified interval (usually one hour), stop the stream to split the files\
+      setTimeout(function(){
+        wstream.end();
+        console.log('finished!');
+        return;
+      },duration);
+
+    });
   });
+  req.end();
+}
+
+
+// app.set('view engine', 'ejs');
+// app.set('views', 'app/views');
+
+
+app.get("/", function(req, res) {
+    res.send("Hello World fhsjak");
 });
 
 // Which port shall we listen on?
@@ -35,6 +138,26 @@ app.set('port', process.env.PORT || 3000 );
 app.use(express.static('app/public'));
 // Init the server
 var server = app.listen(app.get('port'), function() {
-    console.log('SOURCE STREAM: '+url);
   console.log('Hyperlog is running on port ' + app.get('port') + '\nCtrl+C to stop...');
 });
+
+
+// Set the first recording going, then begin recurring recordings
+function init(){
+  // Get the current minutes past the hour
+  var currentMinutes = new Date().getMinutes();
+  // Get the current seconds past the minute
+  var currentSeconds = new Date().getSeconds();
+  // Work out how many milliseconds we should record for to take us up to the start of the next hour
+  var initialDuration = 3600000-((currentMinutes*60)+currentSeconds)*1000;
+  // Make an initial recording until the top of the hour
+  record(initialDuration);
+  console.log('Will record for '+ initialDuration/1000 +" seconds")
+  // Then, begin recurring hourly recordings
+  setTimeout(function(){
+    record(targetDuration);
+  },initialDuration);
+}
+
+// Do ALL THE THINGS
+init();
